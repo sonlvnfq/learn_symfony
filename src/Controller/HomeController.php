ee5dbb0300219a037b6d94fe884ec32533cf0f0f
sonlvnfq <58161209+sonlvnfq@users.noreply.github.com>
2020-06-20 22:48:50 +0700

    add make bundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,10 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
  class HomeController extends AbstractController
  {
      /**
-      * @Route("/", name="front.home")
+      * @Route("/", name="front.home", methods="GET")
       */
      public function index()
      {
          return $this->render('front/home.html.twig');
+     }
+
+     /**
+      * @Route("/product/{product}", name="front.product.detail")
+      */
+     public function detail()
+     {
+         return $this->render('front/product-detail.html.twig', ['name' => 'Fruit']);
      }
  }
